93c0ff5b49faef63b0af82258e5581bcffa2f417
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-27 12:36:04 -0400

    spelling: tooltip
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- superset-frontend/src/components/MetadataBar/MetadataBar.tsx
+++ superset-frontend/src/components/MetadataBar/MetadataBar.tsx
@@ -109,8 +109,8 @@ const StyledItem = styled.div<{
   `}
 `;
 
-// Make sure big tootips are truncated
-const TootipContent = styled.div`
+// Make sure big tooltips are truncated
+const TooltipContent = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 20;
   -webkit-box-orient: vertical;
@@ -160,7 +160,7 @@ const Item = ({
   return isTruncated || collapsed || (tooltip && tooltip !== title) ? (
     <Tooltip
       placement={tooltipPlacement}
-      title={<TootipContent>{tooltip}</TootipContent>}
+      title={<TooltipContent>{tooltip}</TooltipContent>}
     >
       {content}
     </Tooltip>
